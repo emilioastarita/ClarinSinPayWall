b4f1aeddf4688ae55a78c8e8bec293effd560d54
chulo <emilio.astarita@gmail.com>
2015-02-09 15:46:26 -0300

    Se chequea la existencia de jQuery.

--- lanacion.js
+++ lanacion.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   var code = [
-    '$(document.body).on("shown.bs.modal", ".modal" , function (e) { $(e.currentTarget).find("#iframe-registracion").length && $(e.currentTarget).modal("hide"); }); '
+    'jQuery && $(document.body).on("shown.bs.modal", ".modal" , function (e) { $(e.currentTarget).find("#iframe-registracion").length && $(e.currentTarget).modal("hide"); }); '
   ].join('\n');
   var script = document.createElement('script');
   script.textContent = code;
