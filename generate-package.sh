543e8c9c54454406c0e81cf2ef2b463e618bc219
Emilio Astarita <emilio@workana.com>
2016-07-23 20:50:39 -0300

    Ignored releases dir on package generation

--- generate-package.sh
+++ generate-package.sh
@@ -3,6 +3,6 @@
 # Generates a new package release
 #
 VERSION=`cat manifest.json | jq '.version' | tr -d '"'`
-zip -r releases/clarin-sin-paywal.$VERSION.zip ./ -x *.git* generate-package.sh README.md releases/* *.idea*
+zip -r releases/clarin-sin-paywal.$VERSION.zip ./ -x *.git* generate-package.sh README.md *releases/* *.idea*
 
 
